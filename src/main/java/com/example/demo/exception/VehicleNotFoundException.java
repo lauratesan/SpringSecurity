ec96e438e1064abab407be07e1b7915c1c65b004
Laurate Sanejo <maysanejo97@gmail.com>
2020-01-27 21:04:19 +0000

    UserInfoController has been added

--- src/main/java/com/example/demo/exception/VehicleNotFoundException.java
+++ src/main/java/com/example/demo/exception/VehicleNotFoundException.java
@@ -1,10 +1,12 @@
 package com.example.demo.exception;
 
 public class VehicleNotFoundException extends RuntimeException {
+
     public VehicleNotFoundException() {
+
     }
 
-    public VehicleNotFoundException(Long vehicleId ) {
-        super("Vehicle: " +vehicleId +" not found.");
+    public VehicleNotFoundException(Long vehicleId) {
+        super("Vehicle: " + vehicleId + " not found.");
     }
 }
