f901c6499bfcb9524b8c1d94f0e7eecc9629134e
Laurate Sanejo <maysanejo97@gmail.com>
2020-01-27 21:09:31 +0000

    AbstractAuditableEntity and AbstractPersistableEntity...

--- src/main/java/com/example/demo/exception/RestExceptionHandler.java
+++ src/main/java/com/example/demo/exception/RestExceptionHandler.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.http.ResponseEntity.notFound;
+import static org.springframework.http.ResponseEntity.status;
+
 @RestControllerAdvice
 @Slf4j
 public class RestExceptionHandler {
@@ -15,4 +19,10 @@ public class RestExceptionHandler {
         log.debug("handling VehicleNotFoundException...");
         return notFound().build();
     }
+//
+//    @ExceptionHandler(value = {InvalidJwtAuthenticationException.class})
+//    public ResponseEntity invalidJwtAuthentication(InvalidJwtAuthenticationException ex, WebRequest request) {
+//        log.debug("handling InvalidJwtAuthenticationException...");
+//        return status(UNAUTHORIZED).build();
+// }
 }
